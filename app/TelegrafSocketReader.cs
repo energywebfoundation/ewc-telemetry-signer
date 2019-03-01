cacc272e2edf4772dea4c0d97a85946800e5909d
Markus Keil <markus.keil@slock.it>
2019-03-01 11:48:00 +0100

    ass socket reader tests

--- app/TelegrafSocketReader.cs
+++ app/TelegrafSocketReader.cs
@@ -7,7 +7,6 @@ namespace TelemetrySigner
     public class TelegrafSocketReader
     {
         private readonly string _namedPipe;
-        private readonly bool _shouldExit;
 
         public TelegrafSocketReader(string socketPath)
         {
@@ -17,7 +16,6 @@ namespace TelemetrySigner
             }
             
             _namedPipe = socketPath;
-            _shouldExit = false;
         }
 
         public void Read(ConcurrentQueue<string> telemetryQueue)
@@ -26,14 +24,15 @@ namespace TelemetrySigner
             {
                 throw new ArgumentNullException(nameof(telemetryQueue),"Queue can't be null");
             }
-            
-            StreamReader socketStream = File.OpenText(_namedPipe);
-            
-            while (!_shouldExit)
+
+            using (StreamReader socketStream = File.OpenText(_namedPipe))
             {
-                // read forever from pipe
-                string line = socketStream.ReadLine();
-                telemetryQueue.Enqueue(line);
+                while (!socketStream.EndOfStream)
+                {
+                    // read forever from pipe
+                    string line = socketStream.ReadLine();
+                    telemetryQueue.Enqueue(line);
+                }
             }
         }
     }
---
+++ tests/TelegrafSocketReaderTests.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net;
+using FluentAssertions;
+using TelemetrySigner;
+using Xunit;
+
+namespace tests
+{
+    public class TelegrafSocketReaderTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("    ")]
+        [InlineData("foobar")]
+        public void ShouldThrowOnInvalidFilename(string path)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                TelegrafSocketReader tsr = new TelegrafSocketReader(path);
+            });
+        }
+
+        [Fact]
+        public void ShouldThrowOnNullQueue()
+        {
+            string testFile = Path.GetTempFileName();
+            TelegrafSocketReader tsr = new TelegrafSocketReader(testFile);
+            Assert.Throws<ArgumentNullException>(() => tsr.Read(null));
+        }
+
+        [Fact]
+        public void ShouldEnqueueLinesFromFile()
+        {
+            string testFile = Path.GetTempFileName();
+
+            var testLines = new[]
+            {
+                "line-1",
+                "line-2",
+                "line-3",
+                "line-4",
+            };
+            
+            ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+            
+            // Write some lines into TestFile
+            File.WriteAllLines(testFile,testLines);
+            TelegrafSocketReader tsr = new TelegrafSocketReader(testFile);
+            tsr.Read(queue);
+            
+            // dequeue and see if matches
+            var actualArray = queue.ToArray();
+            actualArray.Should().NotBeEmpty()
+                .And.HaveCount(testLines.Length)
+                .And.ContainInOrder(testLines)
+                .And.ContainItemsAssignableTo<string>();
+
+
+        }
+    }
+}
