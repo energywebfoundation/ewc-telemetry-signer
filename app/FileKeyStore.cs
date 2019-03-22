772ad98a16532f8edaa986b756b861ed61bf7fe8
Markus Keil <markus.keil@slock.it>
2019-03-22 09:45:57 +0100

    document keystore

--- app/FileKeyStore.cs
+++ app/FileKeyStore.cs
@@ -3,11 +3,20 @@ using System.IO;
 
 namespace TelemetrySigner
 {
+    /// <summary>
+    /// Implements a file based keystore to store the encrypted signing key and salt
+    /// </summary>
     public class FileKeyStore : IKeyStore
     {
         private readonly string _pkFile;
         private readonly string _saltFilePath;
 
+        /// <summary>
+        /// Create a new instance
+        /// </summary>
+        /// <param name="basePath">Path to store/read the files in/from</param>
+        /// <exception cref="ArgumentException">Path is empty or null</exception>
+        /// <exception cref="DirectoryNotFoundException">The given path is not an existing directory</exception>
         public FileKeyStore(string basePath)
         {
 
@@ -24,21 +33,38 @@ namespace TelemetrySigner
             _pkFile = Path.Combine(basePath, "signing.key");
             _saltFilePath = Path.Combine(basePath, "signing.salt");
         }
+        
+        /// <summary>
+        /// Save the given bytes to the signing.salt file
+        /// </summary>
+        /// <param name="salt"></param>
         public void SaveSalt(byte[] salt)
         {
             File.WriteAllBytes(_saltFilePath,salt);
         }
 
+        /// <summary>
+        /// Load the salt as bytes from the signing.salt file
+        /// </summary>
+        /// <returns></returns>
         public byte[] LoadSalt()
         {
             return File.ReadAllBytes(_saltFilePath);
         }
 
+        /// <summary>
+        /// Load the encrypted key from the signing.key file
+        /// </summary>
+        /// <returns></returns>
         public byte[] LoadEncryptedKey()
         {
             return File.ReadAllBytes(_pkFile);
         }
 
+        /// <summary>
+        /// Save the given bytes to the signing.key file
+        /// </summary>
+        /// <param name="key"></param>
         public void SaveEncryptedKey(byte[] key)
         {
             File.WriteAllBytes(_pkFile,key);
--- app/IKeyStore.cs
+++ app/IKeyStore.cs
@@ -1,11 +1,32 @@
 namespace TelemetrySigner
 {
+    /// <summary>
+    /// Interface that describes a store to persist a key and a salt
+    /// </summary>
     public interface IKeyStore
     {
+        /// <summary>
+        /// Method to save the salt to persistence
+        /// </summary>
+        /// <param name="salt">salt as bytes</param>
         void SaveSalt(byte[] salt);
+        
+        /// <summary>
+        /// Load the salt from persistence
+        /// </summary>
+        /// <returns></returns>
         byte[] LoadSalt();
 
+        /// <summary>
+        /// Load the key from persistence
+        /// </summary>
+        /// <returns></returns>
         byte[] LoadEncryptedKey();
+        
+        /// <summary>
+        /// Method to save the key to persistence
+        /// </summary>
+        /// <param name="key">key as bytes</param>
         void SaveEncryptedKey(byte[] key);
     }
 }
