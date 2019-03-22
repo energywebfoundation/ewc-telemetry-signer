364a73f41100c647d0c6534cb9a7dbff4139dd92
Markus Keil <markus.keil@slock.it>
2019-03-22 10:20:41 +0100

    document telegraf reader

--- app/TelegrafSocketReader.cs
+++ app/TelegrafSocketReader.cs
@@ -4,10 +4,18 @@ using System.IO;
 
 namespace TelemetrySigner
 {
+    /// <summary>
+    /// Reads telemetry from a named-pipe
+    /// </summary>
     public class TelegrafSocketReader
     {
         private readonly string _namedPipe;
 
+        /// <summary>
+        /// Get a new pipe reader
+        /// </summary>
+        /// <param name="socketPath">Path to the named pipe</param>
+        /// <exception cref="ArgumentException">Path does not exist</exception>
         public TelegrafSocketReader(string socketPath)
         {
             if (!File.Exists(socketPath))
@@ -18,6 +26,11 @@ namespace TelemetrySigner
             _namedPipe = socketPath;
         }
 
+        /// <summary>
+        /// Read from the pipe. Blocking.
+        /// </summary>
+        /// <param name="telemetryQueue">Queue to append read telemetry to</param>
+        /// <exception cref="ArgumentNullException">Queue is not initialized</exception>
         public void Read(ConcurrentQueue<string> telemetryQueue)
         {
             if (telemetryQueue == null)
@@ -27,6 +40,7 @@ namespace TelemetrySigner
 
             using (StreamReader socketStream = File.OpenText(_namedPipe))
             {
+                // Read and block
                 while (!socketStream.EndOfStream)
                 {
                     // read forever from pipe
